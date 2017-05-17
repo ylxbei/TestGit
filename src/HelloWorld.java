c9f66220b8dc091983a64b5956a71a5ec450a38d
lz <875944253@qq.com>
2017-05-17 18:00:41 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by Administrator on 2017/5/17.
+ */
+public class HelloWorld {
+     public static  void main(String[] args){
+         System.out.println("Hello Bean");
+         List<String> xin = new ArrayList<>();
+     }
+}
